d4ab934b987b05b83e0e2bcf43ddc30a7ff27422
gabygabylagaby <gabrielagv7656@gmail.com>
2025-04-10 16:25:08 -0400

    feat: domain layer

---
+++ apps/CaseFiles/CaseFiles.Domain/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace CaseFiles.Domain.Models;
+
+public abstract class BaseEntity <TId>
+{
+    public required TId Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ apps/CaseFiles/CaseFiles.Domain/Models/LegalSubmatterType.cs
@@ -0,0 +1,17 @@
+namespace CaseFiles.Domain.Models;
+
+public enum LegalSubmatterType
+{
+    Inheritance,
+    Custody,
+    Divorce,
+    Alimony,
+    
+    UnjustDismissal,
+    Compensation,
+    
+    ContractDispute,
+    PropertyDispute,
+    
+    Other
+} 
