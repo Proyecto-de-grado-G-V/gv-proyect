ba8e2d13ad7f934169ce5e53ebb9d8845e5ff23a
gabygabylagaby <gabrielagv7656@gmail.com>
2025-04-24 19:08:01 -0400

    feat: domain laayer

---
+++ apps/LegalProcessQuoting/LegalProcessQuoting.Domain/Models/LegalSubmatterType.cs
@@ -0,0 +1,11 @@
+namespace LegalProcessQuoting.Domain.Models;
+
+public enum LegalSubmatterType
+{
+    Inheritance = 0,
+
+    Custody = 1,
+    Divorce = 2,
+
+    Femicide = 3
+} 
---
+++ apps/LegalProcessQuoting/LegalProcessQuoting.Domain/Models/SubProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace LegalProcessQuoting.Domain.Models;
+
+public class SubProcess : BaseEntity<int>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    
+    public int LegalProcessId { get; set; }
+    public LegalProcess LegalProcess { get; set; }
+}
