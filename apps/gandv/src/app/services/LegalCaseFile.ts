3d65d96763a310ef243a11d5785144ad23bf8f9c
gabygabylagaby <gabrielagv7656@gmail.com>
2025-04-24 21:55:41 -0400

    feature: legal case file service

---
+++ apps/gandv/src/app/services/LegalCaseFile.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { LegalMatterType } from '../types/Enums';
+
+export const fetchLegalCaseFiles = async (): Promise<Expediente[]> => {
+  try {
+    const response = await axios.get<Expediente[]>('/api/caseFiles/');
+
+    if (Array.isArray(response.data)) {
+      return response.data.map(expediente => ({
+        ...expediente,
+        matter: LegalMatterType[expediente.matterType], 
+      }));
+    } else {
+      console.error('Invalid response format');
+      return [];
+    }
+  } catch (error: any) {
+    console.error('Error fetching case files:', error.message || error);
+    return [];
+  }
+};
