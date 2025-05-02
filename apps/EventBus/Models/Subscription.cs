4254c80a651e40073a90a9f9961d1ce04553a317
gabygabylagaby <gabrielagv7656@gmail.com>
2025-05-02 10:00:00 -0400

    feat: event bus models and repositories

---
+++ apps/EventBus/Models/Subscription.cs
@@ -0,0 +1,17 @@
+﻿namespace EventBus;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Subscription
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("EventType")]
+    public string EventType { get; set; } = string.Empty;
+
+    [BsonElement("CallbackUrl")]
+    public string CallbackUrl { get; set; } = string.Empty;
+}
